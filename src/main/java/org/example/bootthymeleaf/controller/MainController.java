19070a9b9f0bd472f55277c584734ffb4804e91c
dochmai382 <dochmai382@gmail.com>
2025-04-09 12:24:54 +0900

    reset버튼

--- src/main/java/org/example/bootthymeleaf/controller/MainController.java
+++ src/main/java/org/example/bootthymeleaf/controller/MainController.java
@@ -6,6 +6,8 @@ import org.example.bootthymeleaf.model.repository.WordRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,10 +17,20 @@ public class MainController {
 
     @GetMapping
     public String index(Model model) {
-        Word word = new Word();
-        word.setText("고양이");
-        wordRepository.save(word);
+//        Word word = new Word();
+//        word.setText("고양이");
+//        wordRepository.save(word);
         model.addAttribute("words",wordRepository.findAll());
         return "index";
+    }
+
+    @PostMapping("/reset")
+    public String resetWords(RedirectAttributes redirectAttributes) {
+        wordRepository.deleteAll();
+        // Model -> forward
+//        redirectAttributes.addAttribute("message", "초기화 완료");
+        // 주소창을 통해서 전달 message -> parameter
+        redirectAttributes.addFlashAttribute("message", "끝말잇기 기록 삭제!"); // 바로 model로 넣어줌
+        return "redirect:/";
     }
 }
